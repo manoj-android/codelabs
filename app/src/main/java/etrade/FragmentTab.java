0faf7c58193e5b80dc935d4da3bd117e9b0d1dfb
machari <manoj.achari@etrade.com>
2018-10-03 23:43:34 -0700

    produce test crash.

---
+++ app/src/main/java/etrade/FragmentTab.java
@@ -0,0 +1,62 @@
+package etrade;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.machari.firebasesso.R;
+
+public class FragmentTab extends Fragment {
+    RecyclerView mRecyclerView;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_tab, container, false);
+
+        mRecyclerView = (RecyclerView) view.findViewById(R.id.recycler_view);
+        LinearLayoutManager mLayoutManager = new LinearLayoutManager(getActivity());
+        mLayoutManager.setOrientation(RecyclerView.VERTICAL);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        RecyclerView.Adapter mAdapter = new FakePageAdapter(10);
+        mRecyclerView.setAdapter(mAdapter);
+        return view;
+    }
+
+    private static class FakePageAdapter extends RecyclerView.Adapter<FakePageVH> {
+        private final int numItems;
+
+        FakePageAdapter(int numItems) {
+            this.numItems = numItems;
+        }
+
+        @Override
+        public FakePageVH onCreateViewHolder(ViewGroup viewGroup, int i) {
+            View itemView = LayoutInflater.from(viewGroup.getContext())
+                    .inflate(R.layout.list_item_card, viewGroup, false);
+
+            return new FakePageVH(itemView);
+        }
+
+        @Override
+        public void onBindViewHolder(FakePageVH fakePageVH, int i) {
+            // do nothing
+        }
+
+        @Override
+        public int getItemCount() {
+            return numItems;
+        }
+    }
+
+    private static class FakePageVH extends RecyclerView.ViewHolder {
+        FakePageVH(View itemView) {
+            super(itemView);
+        }
+    }
+}
